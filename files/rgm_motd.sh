b308ab0166f04044337caca5bedb6491e63e3b7f
Alex <arocher@orange.fr>
2021-07-07 17:26:00 +0200

    Fix hostname

--- files/rgm_motd.sh
+++ files/rgm_motd.sh
@@ -8,9 +8,9 @@
 BAKIFS=$IFS
 
 function get_ip_fqdn() {
-	host $1 &> /dev/null
+	host "$1" &> /dev/null
 	if [ $? -eq 0 ]; then
-		host $1 | awk '{print $5}'
+		host "$1" | awk '{print $5}' | sed 's/\.$//'
 	else
 		echo '*'
 	fi
@@ -20,11 +20,11 @@ function get_ip_fqdn() {
 alias rgmvenv='. /srv/rgm/python-rgm/bin/activate'
 
 # compute CPU and memory info, display system stats
-CPU_INFO="$(grep processor /proc/cpuinfo | wc -l)x$(grep 'model name' /proc/cpuinfo | uniq | awk -F ':' '{print $2}')"
+CPU_INFO="$(grep -c processor /proc/cpuinfo)x$(grep 'model name' /proc/cpuinfo | uniq | awk -F ':' '{print $2}')"
 MEMORY="$(free -m |grep Mem: | awk -F " " '{print $2}') MB"
 MEMORY_USED="$(free -m |grep Mem: | awk -F " " '{print $3}') MB"
 
-echo -e "\n\e[1m -+- Welcome \e[34mRGM\e[39m server - http://www.rgm-cloud.io -+-\e[0m\n"
+echo -e "\n\e[1m -+- Welcome \e[34mRGM\e[39m server - https://www.rgm-cloud.io -+-\e[0m\n"
 echo -e "Kernel     : \e[97m$(uname -r)\e[0m"
 echo -e "Uptime     : \e[97m$(uptime -p | cut -c 4-)\e[0m"
 echo -e "CPU Info   : \e[97m$CPU_INFO\e[0m"
@@ -33,8 +33,8 @@ echo -e "Memory info: \e[97m$MEMORY ($MEMORY_USED used)\e[0m"
 # display disk occupation (filtering only ext4 and xfs local filesystems)
 IFS=$'\n'
 for item in $(df -hl -t xfs -t ext4 --output=pcent,target | tail -n+2); do
-	PCENT=$(echo $item | awk '{print $1}' | sed 's/%//')
-	MNTPT=$(echo $item | awk '{print $2}')
+	PCENT=$(echo "$item" | awk '{print $1}' | sed 's/%//')
+	MNTPT=$(echo "$item" | awk '{print $2}')
 	COL='\e[32;1m' # green
 	BLD=
 	if [ $PCENT -gt 80 ]; then
@@ -52,7 +52,7 @@ ARIPAD=()
 ARFQDN=()
 # Retrieve the default network interface (the one connected to the default gateway)
 DEFNIC=$(ip route | grep ^default | awk '{print $5}')
-ARIPAD+=("$(ip addr show $DEFNIC | grep 'inet ' | awk '{print $2}' | cut -d '/' -f 1)")
+ARIPAD+=("$(ip addr show "$DEFNIC" | grep 'inet ' | awk '{print $2}' | cut -d '/' -f 1)")
 ARFQDN+=("$(get_ip_fqdn ${ARIPAD[0]})")
 # add secondary network interfaces and retrieve the corresponding FQDN if exists
 for IP in $(ip addr | grep 'inet ' | awk '{print $2}' | cut -d '/' -f 1 | grep -v "\(127.0.0.1\)\|\(${ARIPAD[0]}\)"); do
